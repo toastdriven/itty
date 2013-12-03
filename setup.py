df5aeb4131d48778452e54fee7e018581f02f137
Daniel Lindsley <daniel@toastdriven.com>
2013-12-03 02:04:21 -0800

    Added initial wheel support.

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
-from distutils.core import setup
 import os
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 long_desc = ''
 
